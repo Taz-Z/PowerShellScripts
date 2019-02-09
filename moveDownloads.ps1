1ee35586713b29f637fd3b0ec8e8c6ea8a57d0c0
Taz-Z <tanzim_Zaman@hotmail.com>
2019-02-08 21:11:20 -0500

    Added moveDownloads script
    Moves tv shows/movies to appropriate folders automatically from downloads folder

---
+++ moveDownloads.ps1
@@ -0,0 +1,24 @@
+$downloadsFolder = "D:\ExamplePath"
+$targetFolder = "D:\TvShows"
+Get-ChildItem -Path $downloadsFolder -Filter *.mkv | ForEach-Object { # Can add multiple extensions as well
+
+    #Movies/Shows I download are named as such [NameOfDistributer] Game Of Thrones - S01E1 [1080p]
+    #This makes the folder's name Game Of Thrones
+
+    $nameOfFolder = $_.Name.Substring(
+        $_.Name.IndexOf("]") + 2, 
+        $_.Name.IndexOf("-") - $_.Name.IndexOf("]") - 2
+     )
+     
+    #Full path, in this case D:\TvShows\Game Of Thrones
+    [System.IO.FileInfo]$destinationFolder = (Join-Path -Path $targetFolder -ChildPath $nameOfFolder)
+
+
+     # Create the directory if it doesn't already exits
+    if (!(Test-Path $destinationFolder)) { 
+        New-item -Path $destinationFolder -ItemType Directory 
+    }
+
+    # Move the mkv file to the new/exister folder for the specified show
+    Move-Item -LiteralPath $_.FullName -Destination $destinationFolder.FullName  
+}
