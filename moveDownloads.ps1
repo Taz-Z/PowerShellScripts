c3c3f53080a93811a4849ea05d21f3fb04d6e7f6
Taz-Z <tanzim_Zaman@hotmail.com>
2019-02-08 21:12:53 -0500

    Updated script, fixed typos/documentation

--- moveDownloads.ps1
+++ moveDownloads.ps1
@@ -2,15 +2,13 @@ $downloadsFolder = "D:\ExamplePath"
 $targetFolder = "D:\TvShows"
 Get-ChildItem -Path $downloadsFolder -Filter *.mkv | ForEach-Object { # Can add multiple extensions as well
 
-    #Movies/Shows I download are named as such [NameOfDistributer] Game Of Thrones - S01E1 [1080p]
-    #This makes the folder's name Game Of Thrones
-
-    $nameOfFolder = $_.Name.Substring(
+    #Movies/Shows I download are named as such: [NameOfDistributer] Game Of Thrones - S01E1 [1080p]
+    $nameOfFolder = $_.Name.Substring(     #This makes the folder's name:Game Of Thrones
         $_.Name.IndexOf("]") + 2, 
         $_.Name.IndexOf("-") - $_.Name.IndexOf("]") - 2
      )
      
-    #Full path, in this case D:\TvShows\Game Of Thrones
+    # Full path, in this case: D:\TvShows\Game Of Thrones
     [System.IO.FileInfo]$destinationFolder = (Join-Path -Path $targetFolder -ChildPath $nameOfFolder)
 
 
@@ -19,6 +17,6 @@ Get-ChildItem -Path $downloadsFolder -Filter *.mkv | ForEach-Object { # Can add 
         New-item -Path $destinationFolder -ItemType Directory 
     }
 
-    # Move the mkv file to the new/exister folder for the specified show
+    # Move the video to the new/existing folder for the specified show
     Move-Item -LiteralPath $_.FullName -Destination $destinationFolder.FullName  
 }
